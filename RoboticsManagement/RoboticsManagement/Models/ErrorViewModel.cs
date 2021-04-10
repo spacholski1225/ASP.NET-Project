75dce62e66dd29638eb1083c53cfc68054e7d56d
spacholski1225 <spacholski1225@gmail.com>
2021-04-10 22:31:46 +0200

    start project

---
+++ RoboticsManagement/RoboticsManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoboticsManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
