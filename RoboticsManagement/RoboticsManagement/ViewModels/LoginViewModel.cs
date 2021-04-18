3fa2a6112534ff95cd7aadeac1da3e3412425e35
spacholski1225 <spacholski1225@gmail.com>
2021-04-18 17:47:30 +0200

    created LoginViewModel and Account Controller

---
+++ RoboticsManagement/RoboticsManagement/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoboticsManagement.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm Password does't match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
