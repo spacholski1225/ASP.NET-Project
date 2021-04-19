fe368f0787caf81606ea3a79bca1a809d7c1ae79
spacholski1225 <spacholski1225@gmail.com>
2021-04-19 16:06:00 +0200

    Company ViewModel

--- RoboticsManagement/RoboticsManagement/ViewModels/CompanyRegistartionViewModel.cs
+++ RoboticsManagement/RoboticsManagement/ViewModels/CompanyRegistartionViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RoboticsManagement.ViewModels
 {
-    public class CompanyInfoViewModel
+    public class CompanyRegistartionViewModel
     {
         [Required]
         [EmailAddress]
