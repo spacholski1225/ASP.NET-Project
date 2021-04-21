ce32edc2031c8329d5c148367b534e5cdd63a8a0
spacholski1225 <spacholski1225@gmail.com>
2021-04-21 12:20:25 +0200

    created feature to register new employee

---
+++ RoboticsManagement/RoboticsManagement/Controllers/AdministrationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RoboticsManagement.Data;
+using RoboticsManagement.Models;
+using RoboticsManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoboticsManagement.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdministrationController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(EmployeeRegistrationViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new ApplicationUser //TODO extend about employee informations
+                {
+                    UserName = model.UserName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Success", "Success");
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ RoboticsManagement/RoboticsManagement/ViewModels/EmployeeRegistrationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoboticsManagement.ViewModels
+{
+    public class EmployeeRegistrationViewModel //todO extend about emplyee informations
+    {
+        [Required(ErrorMessage="User name is required")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password, ErrorMessage = "Invalid password")]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage ="Password and Confirm Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
