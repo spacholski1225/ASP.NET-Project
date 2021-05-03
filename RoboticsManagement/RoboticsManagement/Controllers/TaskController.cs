b6c2c30f227e451d4ce4929b2c9bf21559e86825
spacholski1225 <spacholski1225@gmail.com>
2021-05-03 12:35:22 +0200

    added notes on what to do to get a list of finished tasks with information about the company

--- RoboticsManagement/RoboticsManagement/Controllers/TaskController.cs
+++ RoboticsManagement/RoboticsManagement/Controllers/TaskController.cs
@@ -28,18 +28,19 @@ namespace RoboticsManagement.Controllers
         }
         public async Task<IActionResult> FinishedTasks()
         {
-            var listToReturn = new List<TaskViewModel>();
-            var tasks = new List<FormModel>();
+            var listToReturn = new List<TaskViewModel>(); // lista z danymi Company
+            var tasks = new List<FormModel>(); //do zmiany <- tabela TaskForEmployee zwierajaca Id skonczonego zadania wraz z osoba ktora je zrobila
             var doneTasks = _context.EmployeeTasks.Where(x => x.isDone == true).ToList();
             doneTasks.ForEach(t =>
             {
-                tasks.Add(_context.complaintFormModels.FirstOrDefault(c => (c.Id == t.Id) && (c.ApplicationUser.Id != null)));
+             //dla kazdego taska szukamy EmployeeId w TaskForEmployee a nastepnie d zapisaujemy to  do listy tasks
             });
             tasks.ForEach(t =>
             {
-                var user = _userManager.FindByIdAsync(t.ApplicationUser.Id);
+                //tutaj powinna znalezc sie tworzenie listy z danymi Company znaleznione poprzez wykonane zadanie z listy tasks
+                //trzeba zmienic strukture tabel tak aby EmployeeTasks zawieralo Id firmy ktora stworzyla to zadanie, albo zmiana jakos w logice bo teraz 
+                //wydaje mi sie ze nie da sie dostac do aktualnego Id w ApplicationUserId w tabeli complaintFormModels
             });
-            //do skonczenia po naprawieniu dodawania zadania przez administrationcontroller
             return View();
         }
 
