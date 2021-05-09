60c41e920fdee32e134b7db982c234ff0f3c870d
spacholski1225 <spacholski1225@gmail.com>
2021-05-09 17:02:02 +0200

    deleted useless stuff in tests solution

--- RoboticsManagement/RoboticsManagement.Test/ControllerTests/AccountControllerTest.cs
+++ RoboticsManagement/RoboticsManagement.Test/ControllerTests/AccountControllerTest.cs
@@ -16,40 +16,6 @@ namespace RoboticsManagement.Test.ControllerTests
 {
     public class AccountControllerTest
     {
-        private readonly AccountController _sut;
-        private readonly Mock<ILogger<AccountController>> _mockLogger;
-        private readonly Mock<IPrincipal> mockPrincipal;
-        private readonly Mock<HttpContext> mockHttpContext;
-
-
-        public AccountControllerTest()
-        {
-
-            var user = new ApplicationUser
-            {
-                UserName = "test",
-                Id = "1"
-            };
-
-            var claims = new List<Claim>() {
-                new Claim(ClaimTypes.Name, user.UserName),
-                new Claim(ClaimTypes.NameIdentifier, user.Id),
-                new Claim("name", user.UserName)
-            };
-            var identity = new ClaimsIdentity(claims, "Test");
-            var claimsPrincipal = new ClaimsPrincipal(identity);
-
-            mockPrincipal.Setup(x => x.Identity).Returns(identity);
-            mockPrincipal.Setup(x => x.IsInRole(It.IsAny<string>())).Returns(true);
-
-            mockHttpContext.Setup(m => m.User).Returns(claimsPrincipal);
-            
-            //finish it
-        }
-        public Mock<UserManager<ApplicationUser>> GetMockUserManager()
-        {
-            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
-            return new Mock<UserManager<ApplicationUser>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
-        }
+        
     }
 }
