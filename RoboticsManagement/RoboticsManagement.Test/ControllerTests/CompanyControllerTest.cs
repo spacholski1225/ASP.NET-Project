6d270c9a725a2c086a2dbefcbbf7a17527724fa6
spacholski1225 <spacholski1225@gmail.com>
2021-05-12 12:26:13 +0200

    Created class CompanyControllerTest with initial configuration

---
+++ RoboticsManagement/RoboticsManagement.Test/ControllerTests/CompanyControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RoboticsManagement.Configuration;
+using RoboticsManagement.Controllers;
+using RoboticsManagement.Interfaces.IRepository;
+using RoboticsManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RoboticsManagement.Test.ControllerTests
+{
+    public class CompanyControllerTest
+    {
+        private readonly CompanyController _controller;
+        private readonly Mock<UserManager<ApplicationUser>> mockUserManager;
+        private readonly Mock<ILogger<CompanyController>> mockLogger;
+        private readonly Mock<IFormRepository> mockFormRepository;
+        private readonly Mock<AutoMapperConfig> mockMapper;
+        public CompanyControllerTest()
+        {
+            mockLogger = new Mock<ILogger<CompanyController>>();
+            mockFormRepository = new Mock<IFormRepository>();
+            mockMapper = new Mock<AutoMapperConfig>();
+
+            var mockIUserStore = new Mock<IUserStore<ApplicationUser>>();
+            mockUserManager = new Mock<UserManager<ApplicationUser>>(mockIUserStore.Object,
+                null, null, null, null, null, null,null,null);
+            _controller = new CompanyController(mockUserManager.Object, mockLogger.Object,
+                mockFormRepository.Object, mockMapper.Object);
+        }
+
+    }
+}
