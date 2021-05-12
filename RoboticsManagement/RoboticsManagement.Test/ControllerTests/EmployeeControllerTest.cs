12ece0eb4a8f6699de40dbc69c4f4c398003d139
spacholski1225 <spacholski1225@gmail.com>
2021-05-12 13:43:16 +0200

    Created initial class for EmployeeControler unit tests

---
+++ RoboticsManagement/RoboticsManagement.Test/ControllerTests/EmployeeControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RoboticsManagement.Configuration;
+using RoboticsManagement.Controllers;
+using RoboticsManagement.Interfaces.IRepository;
+using RoboticsManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboticsManagement.Test.ControllerTests
+{
+    public class EmployeeControllerTest
+    {
+        private readonly EmployeeController _controller;
+        private readonly Mock<UserManager<ApplicationUser>> mockUserManager;
+        private readonly Mock<ILogger<EmployeeController>> mockLogger;
+        private readonly Mock<IEmployeeTaskRepository> mockEmployeeTaskRepository;
+        private readonly Mock<ITaskForEmployeeRepository> mockTaskForEmployeeRepository;
+        public EmployeeControllerTest()
+        {
+            mockLogger = new Mock<ILogger<EmployeeController>>();
+            mockTaskForEmployeeRepository = new Mock<ITaskForEmployeeRepository>();
+            mockEmployeeTaskRepository = new Mock<IEmployeeTaskRepository>();
+
+            var mockIUserStore = new Mock<IUserStore<ApplicationUser>>();
+            mockUserManager = new Mock<UserManager<ApplicationUser>>(mockIUserStore.Object,
+                null, null, null, null, null, null, null, null);
+            _controller = new EmployeeController(null, mockUserManager.Object,
+                mockTaskForEmployeeRepository.Object, mockEmployeeTaskRepository.Object,
+                mockLogger.Object);
+        }
+    }
+}
